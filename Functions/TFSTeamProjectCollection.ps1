e2e817480f3993bf123f3646dbc55cd02029f53e
kmaddox86 <kendall.maddox@gmail.com>
2015-09-10 22:37:09 -0500

    Add function to Validate URI and Unit Test for that function
    Added a function to validate URIs using .Net methods.
    Added a unit test for this function, will need to write more tests.
    Have stub of function to retrieve team project collections from TFS
    Server

---
+++ Functions/TFSTeamProjectCollection.ps1
@@ -0,0 +1,19 @@
+﻿function Get-TFSTeamProjectCollection
+{
+    Param(
+        [Parameter(Mandatory=$True)]
+        [String]$TFSServer
+    )
+    #[string]::
+}
+
+function isUri
+{
+    Param
+    (
+        [Parameter(Mandatory=$True)]
+        [String]$Uri        
+    )
+    $validate = [Uri]$Uri
+    $validate.IsAbsoluteUri -ne $false -and $validate.Scheme -match '[http|https]'
+}
---
+++ Functions/TFSTeamProjectCollection.tests.ps1
@@ -0,0 +1,8 @@
+﻿$srcFile = '.\TFSTeamProjectCollection.ps1'
+. $srcFile
+
+Describe isUri{
+    It 'validates a web URI' {
+        isUri -Uri 'http://www.google.com' | Should Be $True
+    }
+}
